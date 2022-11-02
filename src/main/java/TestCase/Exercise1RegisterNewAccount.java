7d6471bac4a6223178ab0ff6636bf29589553d29
linhpham2412 <blue.linhpham2412@gmail.com>
2022-11-03 00:26:27 +0700

    finish till enter personal infomatin DOB

--- src/main/java/TestCase/Exercise1RegisterNewAccount.java
+++ src/main/java/TestCase/Exercise1RegisterNewAccount.java
@@ -4,7 +4,7 @@ import nt.tshape.BaseClass;
 import nt.tshape.Constant;
 import nt.tshape.PageModal.AutomationPracticeIndexPage;
 import org.testng.annotations.Test;
-import static nt.tshape.Utils.generateTestEmail;
+import static nt.tshape.Utils.*;
 
 public class Exercise1RegisterNewAccount extends BaseClass {
     @Test
@@ -15,6 +15,12 @@ public class Exercise1RegisterNewAccount extends BaseClass {
                 .clickLinkButtonByName("Sign in")
                 .inputToEmailAddressWithEmail(generateTestEmail())
                 .clickCreateAnAccountButton()
-                .selectDOBFieldIDWithValue(Constant.DOB_DAY_ID,"11");
+                .selectPersonalTitleAs(generateRandomNumberInRange(1,2))
+                .inputPersonalInformationFieldNameWithValue("First name", generateRandomTestCharacters(5))
+                .inputPersonalInformationFieldNameWithValue("Last name", generateRandomTestCharacters(7))
+                .inputPersonalInformationFieldNameWithValue("Password", generateRandomTestCharacters(5))
+                .selectDOBFieldIDWithValue(Constant.DOB_DAY_ID,generateRandomNumberInRange(1,31))
+                .selectDOBFieldIDWithValue(Constant.DOB_MONTH_ID,generateRandomNumberInRange(1,12))
+                .selectDOBFieldIDWithValue(Constant.DOB_YEAR_ID,generateRandomNumberInRange(1900,2022));
     }
 }
--- src/main/java/nt/tshape/PageModal/AutomationPracticeIndexPage.java
+++ src/main/java/nt/tshape/PageModal/AutomationPracticeIndexPage.java
@@ -1,5 +1,6 @@
 package nt.tshape.PageModal;
 
+import nt.tshape.Constant;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +12,9 @@ public class AutomationPracticeIndexPage {
     public WebDriver driver;
     private final WebDriverWait wait;
 
+    private final String titleMRFieldId = "id_gender1";
+    private final String titleMRsFieldId = "id_gender2";
+
     //constructor
     public AutomationPracticeIndexPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -21,40 +25,51 @@ public class AutomationPracticeIndexPage {
     private final String linkXPathByName = "//a[contains(@class,'login') and normalize-space(text()) = '%s']";
     private final By createNewAccountButton = By.id("SubmitCreate");
     private final By emailCreateTextBox = By.id("email_create");
-    private final By titleMRCheckBox = By.id("id_gender1");
-    private final By titleMRsCheckBox = By.id("id_gender2");
-    private final String personalInfoTextFieldLocatorByName = "//label[contains (@for,'customer_firstname') and normalize-space(text())='%s']//parent::div//input";
+    private final String titleCheckBoxLocatorById = "//input[@id='%s']";
+    private final String personalInfoTextFieldLocatorByName = "//label[normalize-space(text())='%s']//parent::div//child::input";
     private final String yourAddressTextFieldLocatorByName = "//label[normalize-space(text())='%s']//parent::p//child::input";
-    private final String dobDropDownFieldByXPath = "//select[@id='%s']";
-    public AutomationPracticeIndexPage goToPageByURL(String locationURL){
+    private final String dobDropDownFieldByXPath = "//div[@id='%s']";
+
+    public AutomationPracticeIndexPage goToPageByURL(String locationURL) {
         driver.get(locationURL);
         return this;
     }
 
-    public AutomationPracticeIndexPage clickLinkButtonByName(String linkName){
-        WebElement linkToBeClick = driver.findElement(By.xpath(String.format(linkXPathByName,linkName)));
+    public AutomationPracticeIndexPage clickLinkButtonByName(String linkName) {
+        WebElement linkToBeClick = driver.findElement(By.xpath(String.format(linkXPathByName, linkName)));
         wait.until(ExpectedConditions.elementToBeClickable(linkToBeClick));
         linkToBeClick.click();
         return this;
     }
 
-    public AutomationPracticeIndexPage clickCreateAnAccountButton(){
+    public AutomationPracticeIndexPage clickCreateAnAccountButton() {
         wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(createNewAccountButton)));
         driver.findElement(createNewAccountButton).click();
         return this;
     }
 
-    public AutomationPracticeIndexPage inputToEmailAddressWithEmail(String emailAddress){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(emailCreateTextBox));
+    public AutomationPracticeIndexPage inputToEmailAddressWithEmail(String emailAddress) {
         driver.findElement(emailCreateTextBox).sendKeys(emailAddress);
         return this;
     }
 
-    public AutomationPracticeIndexPage selectDOBFieldIDWithValue(String dobFieldId, String fieldValue){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(dobDropDownFieldByXPath,dobFieldId))));
-        WebElement dobField = driver.findElement(By.xpath(String.format(dobDropDownFieldByXPath,dobFieldId)));
-        Select dobDropDownField = new Select(dobField);
-        dobDropDownField.selectByVisibleText(fieldValue+"  ");
+    public AutomationPracticeIndexPage selectDOBFieldIDWithValue(String dobFieldId, String fieldValue) {
+        Select dobDropDownField = new Select(driver.findElement(By.xpath(String.format(dobDropDownFieldByXPath, dobFieldId)+"//select")));
+        dobDropDownField.selectByValue(fieldValue);
+        return this;
+    }
+
+    public AutomationPracticeIndexPage selectPersonalTitleAs(String titleValue){
+        String titleId = (titleValue.equals("1")) ? titleMRFieldId : titleMRsFieldId;
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(titleCheckBoxLocatorById,titleId))));
+        WebElement titleCheckBox = driver.findElement(By.xpath(String.format(titleCheckBoxLocatorById,titleId)));
+        titleCheckBox.click();
+        return this;
+    }
+
+    public AutomationPracticeIndexPage inputPersonalInformationFieldNameWithValue(String fieldName, String fieldValue){
+        WebElement personalInfoField = driver.findElement(By.xpath(String.format(personalInfoTextFieldLocatorByName,fieldName)));
+        personalInfoField.sendKeys(fieldValue);
         return this;
     }
 }
