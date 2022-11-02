96bca752cf0ff8a36ac3fca8ba29975d5c863f97
Linh Pham <linh.pham@nashtechglobal.com>
2022-11-02 21:12:23 +0700

    working on selectDOBFieldValue need to have same back spaces to be able to select data

--- src/main/java/nt/tshape/Constant.java
+++ src/main/java/nt/tshape/Constant.java
@@ -3,4 +3,7 @@ package nt.tshape;
 public class Constant {
     public static final long SHORT_TIME = 3;
     public static final long LONG_TIME = 10;
+    public static final String DOB_DAY_ID = "days";
+    public static final String DOB_MONTH_ID = "months";
+    public static final String DOB_YEAR_ID = "years";
 }
